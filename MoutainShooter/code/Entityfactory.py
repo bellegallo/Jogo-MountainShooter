f8c944738874ce74d3c60c0bf8b9ce60efc71fb4
Bellegallo <bellegsk2@gmail.com>
2024-04-11 16:11:04 -0300

    Aula de criação de tiro e colisões de tela

--- MoutainShooter/code/Entityfactory.py
+++ MoutainShooter/code/Entityfactory.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import random
-
 from code.Background import Background
-from code.Enemy import Enemy
-from code.Player import Player
-from code.const import WIN_WIDTH, WIN_HEIGHT
+from code.const import WIN_WIDTH
 
 
 class Entityfactory:
@@ -14,20 +10,8 @@ class Entityfactory:
     def get_entity(entity_name: str, position=(0, 0)):
         match entity_name:
             case 'Level1BG':
-                list_bg = []
+                list_bg =[]
                 for i in range(5):
-                    list_bg.append(Background(f'Level1BG{i}', (0, 0)))
-                    list_bg.append(Background(f'Level1BG{i}', (WIN_WIDTH, 0)))
+                   list_bg.append(Background(f'Level1BG{i}', (0,0)))
+                   list_bg.append(Background(f'Level1BG{i}', (WIN_WIDTH,0)))
                 return list_bg
-
-            case 'Player1':
-                return Player('Player1', (10, WIN_HEIGHT / 2 - 30))
-
-            case 'Player2':
-                return Player('Player2', (10, WIN_HEIGHT / 2 + 30))
-
-            case 'Enemy1':
-                return Enemy('Enemy1', (WIN_WIDTH + 10, random.randint(0 + 40, WIN_HEIGHT - 40)))
-
-            case 'Enemy2':
-                return Enemy('Enemy2', (WIN_WIDTH + 10, random.randint(0 + 40, WIN_HEIGHT -40)))
